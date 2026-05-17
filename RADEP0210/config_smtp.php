76043f12cb51e859c95576e1eac6f9fb0dd3eac1
SantiagoVazquez21 <santiagoelianvazquez@gmail.com>
2026-05-16 23:14:28 -0300

    Delete RADEP0210/config_smtp.php
    Eliminar archivo sensible

--- RADEP0210/config_smtp.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_PORT', 587);
-define('SMTP_USER', 'santiagovazquezempresa@gmail.com');
-// Gmail App Passwords are shown with spaces in the UI; use them without spaces here.
-define('SMTP_PASS', 'lbtihauqdfxkkizb');
-define('FROM_EMAIL', 'no-reply@svazquez.local');
-define('FROM_NAME', 'Svazquez - Organización');
-?>
